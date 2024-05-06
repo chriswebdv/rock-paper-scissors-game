96ca6e7a088bfc7488fc2b5c37c5960cfb4ce50b
chriswebdv <chriswebdv@gmail.com>
2024-05-06 16:16:34 -0600

    Add return statement to getHumaChoice function

--- main.js
+++ main.js
@@ -20,5 +20,38 @@ function getComputerChoice() {
 // Write the logic for the human choice using if/else statements
 
 function getHumanChoice () {
-    let humanChoice = prompt("Choose one: rock, paper, scissors ")
+    let humanChoice = prompt("Choose one: rock, paper, scissors ").toLowerCase()
+    return humanChoice
 }
+
+// Single round play
+
+function playRound(humanChoice, computerChoice) {
+    if(computerChoice === "rock" && humanChoice === "paper") {
+        console.log("You win! Paper beats Rock");
+        humanScore++
+    } else if (computerChoice === "paper" && humanChoice === "rock") {
+        console.log("You lose! Paper beats Rock");
+        computerScore++
+    } else if (computerChoice === "rock" && humanChoice === "scissors") {
+        console.log("You lose! Rock beats Scissors");
+        computerScore++
+    } else if (computerChoice === "scissors" && humanChoice === "rock") {
+        console.log("You win! Rock beats Scissors");
+        humanScore++
+    } else if (computerChoice === "paper" && humanChoice === "scissors") {
+        console.log("You win! Scissors beats Paper");
+        humanScore++
+    } else if (computerChoice === "scissors" && humanChoice === "paper") {
+        console.log("You lose! Scissors beats Paper");
+        computerScore++
+    } else {
+        console.log("Tie try again!");
+    }
+        
+}
+
+const humanSelection = getHumanChoice()
+const computerSelection = getComputerChoice()
+
+playRound(humanSelection, computerSelection)
